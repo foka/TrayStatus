b4d2f62b883956f1da77accbc9158be91a360a95
Michał Włodarczyk <michal.foka.wlodarczyk@gmail.com>
2023-01-05 20:02:28 +0100

    Add context menu with close button

--- TrayStatus.Bootstrapper/MainForm.cs
+++ TrayStatus.Bootstrapper/MainForm.cs
@@ -6,11 +6,21 @@ public partial class MainForm : Form
     {
         InitializeComponent();
 
+        var closeApplicationMenuItem = new ToolStripMenuItem("Close");
+        closeApplicationMenuItem.Click += (_, __) => Application.Exit();
+
         NotifyIcon icon = new NotifyIcon()
         {
           Text = "test",
           Visible = true,
           Icon = new Icon("testicon.ico"),
+          ContextMenuStrip = new ContextMenuStrip()
+          {
+            Items =
+            {
+                closeApplicationMenuItem
+            }
+          }
         };
     }
 
