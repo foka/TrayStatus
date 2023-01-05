05d3eb82208d48ef8be1fca2f036a4078878262f
Michał Włodarczyk <michal.foka.wlodarczyk@gmail.com>
2023-01-05 19:55:24 +0100

    Hide main window and add icon

--- TrayStatus.Bootstrapper/MainForm.cs
+++ TrayStatus.Bootstrapper/MainForm.cs
@@ -5,5 +5,19 @@ public partial class MainForm : Form
     public MainForm()
     {
         InitializeComponent();
+
+        NotifyIcon icon = new NotifyIcon()
+        {
+          Text = "test",
+          Visible = true,
+          Icon = new Icon("testicon.ico"),
+        };
+    }
+
+    protected override void OnLoad(EventArgs e)
+    {
+        Visible = false;
+        ShowInTaskbar = false;
+        base.OnLoad(e);
     }
 }
